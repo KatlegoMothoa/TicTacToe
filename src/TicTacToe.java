11a887de01ea7aba1397ff17d7deb8f19b89d347
KatlegoMothoa <kmothoa50@gmail.com>
2025-10-20 22:47:28 +0200

    Customizing the Window

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,4 +1,3 @@
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -7,8 +6,10 @@ public class TicTacToe {
     int boardWidth = 600;
     int boardHeight = 650;
 
-    JFrame frame = new JFrame("i'm tryna memorize these things");
+    JFrame frame = new JFrame("KatKodes Tic-Tac-toe");
     JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
+    JPanel boardPanel = new JPanel();
 
     TicTacToe() {
         frame.setVisible(true);
@@ -17,6 +18,21 @@ public class TicTacToe {
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
+
+        textLabel.setBackground(Color.darkGray);
+        textLabel.setForeground(Color.white);
+        textLabel.setFont(new Font("Arial", Font.BOLD, 50));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Tic-Tac-Toe");
+        textLabel.setOpaque(true);
+
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel);
+        frame.add(textPanel, BorderLayout.NORTH);
+
+        boardPanel.setLayout(new GridLayout(3, 3));
+        boardPanel.setBackground(Color.blue);
+        frame.add(boardPanel);
 
     }
 
